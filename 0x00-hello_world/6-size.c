28e28cf15cd3b66e53a2202845d0c0835ae63c47
Adebem <adeolabusayo1995@gmail.com>
2023-06-21 01:37:45 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	printf("The size of an int is %d bytes.\n", sizeof(int));
-	printf("The size of a float is a %f bytes.\n", sizeof(float));
+	printf("The size of a float is a %d bytes.\n", sizeof(float));
 	printf("The size of a double is a %d bytes.\n", sizeof(double));
 	printf("The size of a char is a %d bytes.\n", sizeof(char));
 	return (0);
