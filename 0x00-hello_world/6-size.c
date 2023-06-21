9e01514631287311fdaa695433493d75b002d4ba
Adebem <adeolabusayo1995@gmail.com>
2023-06-21 01:52:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,6 @@ int main(void)
 {
 	printf("The size of an int is %ld bytes.\n", sizeof(int));
 	printf("The size of a float is a %ld bytes.\n", sizeof(float));
-	printf("The size of a double is a %ld bytes.\n", sizeof(double));
 	printf("The size of a char is a %ld bytes.\n", sizeof(char));
 	printf("The size of a long int is %ld bytes.\n", sizeof(long int));
 	printf("The size of a long long int is %ld bytes.\n", sizeof(long long int));
