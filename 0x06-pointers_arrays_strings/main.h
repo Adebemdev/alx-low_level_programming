517150d282308d1d6d0cc23752ff01151d1c658a
Adebem <adeolabusayo1995@gmail.com>
2023-06-30 18:59:24 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,7 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+// Prototype of the _putchar function
+int _putchar(int character);
 #endif
