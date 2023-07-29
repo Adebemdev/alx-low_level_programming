725d3bdcd7de4f838a3237480933e5eb39d9260a
Adebem <adeolabusayo1995@gmail.com>
2023-07-29 00:49:54 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints the alphabet
  *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-int i, e;
-i = 48;
-e = 48;
-	while (e < 58)
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+	for (i = 0; i < 26; i++)
 	{
-		i = 48;
-		while (i < 58)
-		{
-			if (e != i && e < i)
-			{
-				putchar(e);
-				putchar(i);
-				if (i == 57 && e == 56)
-				{
-					break;
-				}
-				putchar(',');
-				putchar(' ');
-			}
-			i++;
-		}
-		e++;
+		putchar(alp[i]);
 	}
 	putchar('\n');
 return (0);
