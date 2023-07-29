18d714caf899eeb261e158bf4e8809e51ea26a09
Adebem <adeolabusayo1995@gmail.com>
2023-07-29 10:40:29 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - pria string to output
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+char putchar[] = "_putchar";
+int i = 0;
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
