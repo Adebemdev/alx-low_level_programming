21be7167146c2bb9edba2979f698b924a8510ef3
Adebem <adeolabusayo1995@gmail.com>
2023-07-30 16:31:50 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,11 +2,11 @@
 /**
  * main - tests for positive or negative int
  * Return: 0
- */
+*/
 int main(void)
 {
 int i;
 i = 0;
-	positive_or_negative(i);
-	return (0);
+positive_or_negative(i);
+return (0);
 }
