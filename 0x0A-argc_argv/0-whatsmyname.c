7c3a05acb628db7be7576399aaff79ffd279ed87
Adebem <adeolabusayo1995@gmail.com>
2023-08-07 01:02:43 -0700

    argc, argv file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - Program that prints name, followed by a new line
+ * @argc: number pf command-line argument pass to the program
+ * @argv: array of string containing the number of command-line
+ * argument passed
+ * Return: 0 if successful
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
