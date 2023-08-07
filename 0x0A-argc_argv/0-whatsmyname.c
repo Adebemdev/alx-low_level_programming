6829aad1f7aec46b781fd5b16d78bd0d146fbac1
Adebem <adeolabusayo1995@gmail.com>
2023-08-07 01:06:51 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * argument passed
  * Return: 0 if successful
 */
-int main(int argc, char *argv[])
+int main(__attribute__ ((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
