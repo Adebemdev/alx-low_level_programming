1a1de752f4a6206cf572ce4c938bd53db43d90fc
Adebem <adeolabusayo1995@gmail.com>
2023-08-07 18:10:06 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,12 +10,12 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("%s\n", argv[0]);
+		printf("Error\n");
 		return (1);
 	}
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 	int result = num1 * num2;
-printf("The result of the multiplication:%d\n", result);
+	printf("The result of the multiplication:%d\n", result);
 return (0);
 }
