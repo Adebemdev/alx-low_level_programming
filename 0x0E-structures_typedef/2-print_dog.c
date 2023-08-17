5633a4789482f87ebfc5e3829f98bdce528ed89c
Adebem <adeolabusayo1995@gmail.com>
2023-08-17 10:26:25 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,9 +7,15 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	return;
-	printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
-	printf("Age: %.1f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
+char *name, *owner;
+	if (d)
+	{
+		name = (*d).name;
+		owner = (*d).owner;
+		if (!name)
+			name = "(nil)";
+		if (!owner)
+			owner = "(nil)";
+		printf("Name: %s\nAge: %f\nOwner: %s\n", name, (*d).age, owner);
+	}
 }
